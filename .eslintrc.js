4a53328131e27f3abf3a32b0cbb2932baf992f65
yhkim <yhkim@miridih.com>
2022-06-06 00:58:52 +0900

    config: eslint, prettier basic configs

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['react', '@typescript-eslint'],
+    rules: {},
+};
