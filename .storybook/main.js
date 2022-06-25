523bda6664b77da26a1895664abb0c92f9d3b986
yhkim <yhkim@miridih.com>
2022-06-26 01:28:30 +0900

    deps: storybook a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,9 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    // Other Storybook addons
+    '@storybook/addon-a11y'
   ],
   "framework": "@storybook/react"
 }
